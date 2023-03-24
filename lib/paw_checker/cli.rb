f9082e30bc15e992914ceef98f21061f62b93b2a
adorechic <adorechic@gmail.com>
2023-03-24 10:55:31 +0900

    Show dependency

--- lib/paw_checker/cli.rb
+++ lib/paw_checker/cli.rb
@@ -1,10 +1,16 @@
 require 'thor'
+require 'syntax_tree'
 
 module PawChecker
   class CLI < Thor
-    desc "structure", "Run!"
-    def structure
-      puts "Hello"
+    desc "structure [path]", "Run!"
+    def structure(path)
+      source = SyntaxTree.read(path)
+      var_refs = SyntaxTree.search(source, "VarRef")
+      consts = var_refs.select {|node|
+        node.value.instance_of?(SyntaxTree::Const)
+      }
+      p consts.map(&:value).map(&:value).uniq
     end
   end
 end
