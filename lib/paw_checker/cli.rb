976b7b439550bbcbd2dc808fba8954560d64d9b2
adorechic <adorechic@gmail.com>
2023-03-27 14:28:57 +0900

    Show relations

--- lib/paw_checker/cli.rb
+++ lib/paw_checker/cli.rb
@@ -10,7 +10,32 @@ module PawChecker
       consts = var_refs.select {|node|
         node.value.instance_of?(SyntaxTree::Const)
       }
+      puts "#dependency"
       p consts.map(&:value).map(&:value).uniq
+
+      commands = SyntaxTree.search(source, "Command")
+      belongs = commands.select {|node|
+        node.child_nodes.first.value == "belongs_to"
+      }.map {|node|
+        node.child_nodes[1].child_nodes.first.child_nodes.first.value
+      }
+      puts "#belongs_to"
+      p belongs
+      has_manies = commands.select {|node|
+        node.child_nodes.first.value == "has_many"
+      }.map {|node|
+        node.child_nodes[1].child_nodes.first.child_nodes.first.value
+      }
+      puts "#has_many"
+      p has_manies
+
+      has_ones = commands.select {|node|
+        node.child_nodes.first.value == "has_one"
+      }.map {|node|
+        node.child_nodes[1].child_nodes.first.child_nodes.first.value
+      }
+      puts "#has_one"
+      p has_ones
     end
   end
 end
