b0e69bd6b82892a3964825ae3cb8e85422cfef19
adorechic <adorechic@gmail.com>
2023-03-27 15:52:33 +0900

    Extract

--- lib/paw_checker.rb
+++ lib/paw_checker.rb
@@ -1,6 +1,7 @@
 # frozen_string_literal: true
 
 require_relative "paw_checker/version"
+require_relative "paw_checker/class_definition"
 
 module PawChecker
   class Error < StandardError; end
--- lib/paw_checker/cli.rb
+++ lib/paw_checker/cli.rb
@@ -1,48 +1,12 @@
 require 'thor'
 require 'syntax_tree'
+require 'paw_checker'
 
 module PawChecker
   class CLI < Thor
     desc "structure [path]", "Run!"
     def structure(path)
-      source = SyntaxTree.read(path)
-      idx = SyntaxTree.index(source)
-      cls = idx.find {|i|
-        i.instance_of?(SyntaxTree::Index::ClassDefinition)
-      }
-      puts "class name"
-      puts cls.name
-
-      var_refs = SyntaxTree.search(source, "VarRef")
-      consts = var_refs.select {|node|
-        node.value.instance_of?(SyntaxTree::Const)
-      }
-      puts "#dependency"
-      p consts.map(&:value).map(&:value).uniq
-
-      commands = SyntaxTree.search(source, "Command")
-      belongs = commands.select {|node|
-        node.child_nodes.first.value == "belongs_to"
-      }.map {|node|
-        node.child_nodes[1].child_nodes.first.child_nodes.first.value
-      }
-      puts "#belongs_to"
-      p belongs
-      has_manies = commands.select {|node|
-        node.child_nodes.first.value == "has_many"
-      }.map {|node|
-        node.child_nodes[1].child_nodes.first.child_nodes.first.value
-      }
-      puts "#has_many"
-      p has_manies
-
-      has_ones = commands.select {|node|
-        node.child_nodes.first.value == "has_one"
-      }.map {|node|
-        node.child_nodes[1].child_nodes.first.child_nodes.first.value
-      }
-      puts "#has_one"
-      p has_ones
+      ClassDefinition.parse(path)
     end
   end
 end
